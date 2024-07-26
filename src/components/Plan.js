a0b4364b89851bbe2d0180ab23eb706f93178c44
Nazym-MU <nazym@uni.minerva.edu>
2024-07-26 13:49:39 +0500

    Add Plan to the table of contents

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -45,7 +45,7 @@ const Plan = () => {
   };
 
   return (
-    <section className="plan-container" ref={sectionRef}>
+    <section className="plan-container" id='plan' ref={sectionRef}>
       <div className="plan">
         <div className="plan-column left">
           <div className="content" ref={addToRefs}>
